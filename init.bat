a7ad31aa3496baab5916191dc0b176f064d7382d
Owner <dsorrent7@gmail.com>
2020-08-08 19:41:48 -0400

    Ready to push

---
+++ init.bat
@@ -0,0 +1,6 @@
+echo "# hw10-Team-Profile-Generator" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/dsorrent2017/hw10-Team-Profile-Generator.git
+git push -u origin master
